3e5ded91ca3b6d553f69d924a78db69eee4bd0f4
Roald Hacquebord <afroald@gmail.com>
2013-09-13 12:24:49 +0200

    Include Compass

--- src/Sprockets/Engine/ScssEngine.php
+++ src/Sprockets/Engine/ScssEngine.php
@@ -6,6 +6,7 @@ class ScssEngine extends Processor {
     public function process($content)
     {
         $compiler = new \scssc();
+        new \scss_compass($compiler);
 
         return $compiler->compile($content);
     }
